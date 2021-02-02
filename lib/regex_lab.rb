a196113bc26591a5b8fe522129c2ccf7bc36fb84
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-02 03:32:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,8 +11,15 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  !!text.match(/^[A-Z]|[.?!]$/)
-  !text.match(/^[A-Z]|[\w]$/)
+  if !!text.match(/^[A-Z]|[.?!]$/)
+  else
+    false
+  end
+end
+
+
+
+
 end
 
 def valid_phone_number?(phone)
